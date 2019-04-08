fbab191349be32a21a257629a8ea531fad683d99
tw5033 <tw5033@gmail.com>
2019-04-08 19:42:56 +0000

    added request node functions

---
+++ request.js
@@ -0,0 +1,19 @@
+// require `request` and the Node `fs` (filesystem) module
+var request = require('request');
+var fs = require('fs');
+
+request.get('https://sytantris.github.io/http-examples/future.jpg')               // Note 1
+       .on('error', function (err) {                                   // Note 2
+         console.log('An error has occured while requesting from the page.');
+       })
+       .on('response', function (response) {                           // Note 3
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Response message: ', response.statusMessage);
+         console.log('Content Type: ', response.headers['content-type']);
+         console.log('Downloading image...');
+       })
+       .on('end', function (response) {
+          console.log('Download Complete');
+        })
+       .pipe(fs.createWriteStream('./future.jpg'));               // Note 4
+
